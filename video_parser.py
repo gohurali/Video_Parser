09ec7ec739d045071245ddf1c7d6e82b319bf6ef
gohurali <gohurali@live.com>
2018-11-21 00:36:57 -0800

    added documentation and functions for organization

--- video_parser.py
+++ video_parser.py
@@ -1,18 +1,23 @@
 __author__ = 'Gohur Ali'
 __version__ = 1.2
-import cv2
-import os
-import argparse
-import time
-import numpy as np
+import cv2          # Opening video and grabbing frames
+import os           # Directory searching
+import argparse     # Command line arguement seeting
+import time         # Getting current time for naming convention
+import numpy as np  # Python float to Numpy float conversion
 """
 Python script for frame extraction from video files.
 
 How to run:
 python3 video_parser.py <video location> <output directory name> <number of frames to skip>
 
-You must have OpenCV and Numpy to be able to run this program as they are dependencies
-Files are named YYYYMMDDhourMinute_pictureNum.png
+You must have OpenCV and Numpy to be able to run this program as they are dependencies:
+    To install OpenCV:
+        pip install opencv-python
+    To install numpy
+        pip install numpy
+
+Output images are named YYYYMMDDhourMinute_pictureNum.png
 """
 
 parser = argparse.ArgumentParser(description='Process and Cut Video Frames')
@@ -21,46 +26,49 @@ parser.add_argument('output_dir', help='the video path in filesystem')
 parser.add_argument('seconds_delay',help='number of seconds delayed before frame is saved')
 args = parser.parse_args()
 
-video_path = args.input_video
+def get_current_time():
+    minute = time.localtime().tm_min
+    hour = time.localtime().tm_hour
+    day = time.localtime().tm_mday
+    month = time.localtime().tm_mon
+    year = time.localtime().tm_year
+    return minute, hour, day, month, year
 
-frame_count = 0
-vid_cap = cv2.VideoCapture(video_path)
-
-if(os.path.exists(args.output_dir) == False):
-    os.mkdir(args.output_dir)
-else:
-    print('Directory: "',args.output_dir,'" already exists!')
-
-minute = time.localtime().tm_min
-hour = time.localtime().tm_hour
-day = time.localtime().tm_mday
-month = time.localtime().tm_mon
-year = time.localtime().tm_year
-
-while(vid_cap.isOpened()):
-    if(int(args.seconds_delay) == 0):
-        vid_cap.set(cv2.CAP_PROP_POS_MSEC, frame_count)
-    elif(int(args.seconds_delay) > 0):
-        frame_skip_rate = np.float32( int(frame_count) * int(args.seconds_delay) * 1000)
-        vid_cap.set(cv2.CAP_PROP_POS_MSEC,(frame_skip_rate))
+def parse_video(args):
+    video_path = args.input_video
+    frame_count = 0
+    vid_cap = cv2.VideoCapture(video_path)
+    minute, hour, day, month, year = get_current_time()
     
-    ret, frame = vid_cap.read()
-    
-    if(ret == True):
-        im_name = str(year) + str(month) + str(day) + str(hour) + str(minute) + '_'+str(frame_count) + '.png'
-        print('Creating image: ', im_name)
-
-        save_loc = args.output_dir + '/' + im_name
-        cv2.imwrite(save_loc,frame)
+    if(os.path.exists(args.output_dir) == False):
+        os.mkdir(args.output_dir)
     else:
-        break
-    frame_count += 1
+        print('Directory: "',args.output_dir,'" already exists!')
 
-vid_cap.release()
-cv2.destroyAllWindows()
+    while(vid_cap.isOpened()):
+        if(int(args.seconds_delay) == 0):
+            vid_cap.set(cv2.CAP_PROP_POS_MSEC, frame_count)
+        elif(int(args.seconds_delay) > 0):
+            frame_skip_rate = np.float32( int(frame_count) * int(args.seconds_delay) * 1000)
+            vid_cap.set(cv2.CAP_PROP_POS_MSEC,(frame_skip_rate))
+
+        ret, frame = vid_cap.read()
+        
+        if(ret == True):
+            im_name = str(year) + str(month) + str(day) + str(hour) + str(minute) + '_'+str(frame_count) + '.png'
+            print('Creating image: ', im_name)
+
+            save_loc = args.output_dir + '/' + im_name
+            cv2.imwrite(save_loc,frame)
+        else:
+            break
+        frame_count += 1
+
+    vid_cap.release()
+    cv2.destroyAllWindows()
 
 def main():
-    pass
+    parse_video(args)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
